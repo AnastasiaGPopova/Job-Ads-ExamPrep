3f0460ec320d81012f7405348e8fb620820b37ec
AnastasiaGPopova <ana.popova.register@gmail.com>
2023-02-07 20:27:10 +0200

    added skeleton

---
+++ src/services/housingService.js
@@ -0,0 +1,9 @@
+const Housing = require('../models/Housing')
+
+exports.getOneHouse = (houseId) => Housing.findById(houseId)
+exports.getAllHouses = () => Housing.find()
+exports.getLastAdded = () => Housing.find({}).sort({createdAt: -1})
+exports.update = (houseId, data) => Housing.findByIdAndUpdate(houseId, data, {runValidators: true})
+exports.deleteHouse = (houseId) => Housing.findByIdAndDelete(houseId, {runValidators: true})
+exports.getSearchedbyType = (item) => Housing.find({}).where('type').equals(`${item}`)
+exports.createNewHouse = (data) => Housing.create(data)
